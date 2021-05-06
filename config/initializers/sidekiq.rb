5e97b6c66abbb2d15c906c8f47ff09a82c3a44cf
fernando <fernandomirandalolzeiro@gmail.com>
2021-05-06 17:54:38 -0300

    Add configs redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
-
 unless Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"]|| '6379', namespace: "codetriage-sidekiq" }
+    config.redis = {
+      host: ENV['REDIS_HOST'],
+      port: ENV['REDIS_PORT'] || '6379'
+    }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"]|| '6379', namespace: "codetriage-sidekiq" }
+    config.redis = {
+      host: ENV['REDIS_HOST'],
+      port: ENV['REDIS_PORT'] || '6379'
+    }
   end
 end
-
 if Sidekiq.server?
   Rails.application.config.active_record.warn_on_records_fetched_greater_than = 1500
 end
